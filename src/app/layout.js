e4f6858b00e0d2491ea92ef86cee09b9dd6a257c
hadees <hadees621@gmail.com>
2023-11-04 19:14:56 +0500

    chore : excess code removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,6 @@ import { Footer, Navbar } from "@/components/common";
 import { Inter } from 'next/font/google'
 import '@/style/global.css';
 
-import { Inter } from 'next/font/google';
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
