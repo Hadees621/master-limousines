a52d42535f3ddfe96c5bf77f3355c928e852622a
hadees <hadees621@gmail.com>
2023-11-04 22:03:08 +0500

    section implemented

---
+++ src/app/ourfleet/page.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function OurFleet() {
+  return (
+
+    <div class="flex flex-wrap">
+      <div class="bg-black w-full sm:w-full md:w-full lg:w-1/2 lg:p-64 md:p-32 sm:p-24">
+        <div>
+          <h1 className="text-white font-montserrat text-[30px] font-medium tracking-[12px] uppercase text-center mb-24 pt-14">Party Bus</h1>
+          <p className="text-[20px] font-archivo text-center text-white font-light">
+            When it comes to luxury ground transportation, the Mercedes-Benz Sprinter Limo is a top choice for any occasion. Whether you're planning a corporate event or a wedding, this sleek and stylish vehicle is sure to impress. Not only is the interior spacious and comfortable, but the onboard TVs and bar area make it perfect for presentations or celebrations.
+          </p>
+          <br />
+          <br />
+          <p className="text-[20px] font-archivo text-center text-white">
+            At Masterpiece Limousine, we prioritize safety, reliability, and comfort, which is why we offer the Mercedes Sprinter Limo for our clients. Our professionally trained chauffeurs ensure a smooth and enjoyable ride, and we are fully licensed and insured for your peace of mind.
+          </p>
+          <br />
+          <br />
+          <p className="text-[20px] font-archivo text-center text-white">
+            In addition to corporate events and weddings, the Mercedes Sprinter Limo is also a popular choice for proms, bachelor/bachelorette parties, and other special occasions. With light shows and customizable music options, this luxury vehicle adds a touch of glamour to any event.
+          </p>
+          <br />
+          <br />
+          <p className="text-[20px] font-archivo text-center text-white">
+            Trust Masterpiece Limousine to provide you with the highest quality luxury ground transportation services. Contact us today to book the Mercedes Sprinter Limo for your next event and experience the ultimate in style and comfort.
+          </p>
+        </div>
+
+      </div>
+      <div class="w-full sm:w-full md:w-full lg:w-1/2">
+        <img src="/ourfleet/exec-suv.png" alt="Dummy Image" class="w-full h-full" />
+      </div>
+    </div>
+  )
+}
+
+export default OurFleet
