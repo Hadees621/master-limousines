37334a2361a593df6cf5d921e64f3a9e47f9621a
hadees <hadees621@gmail.com>
2023-11-07 05:01:56 +0500

    sections randered in services page

--- src/app/services/components/Section.js
+++ src/app/services/components/Section.js
@@ -1,74 +1,73 @@
-import React from 'react'
+import React from 'react';
 
-const Section = () => {
+const Section = ({ order, textContent, title, imgURL, color, number }) => {
+
+    const totalIcons = 17;
+
+    const filledIcons = Array.from({ length: Math.min(number, totalIcons) }, (_, i) => i);
+
+    const unfilledIcons = Array.from({ length: totalIcons - filledIcons.length }, (_, i) => i);
+
     return (
         <>
-            <div className='bg-black h-[828px] w-full flex justify-center items-center'>
-                <div className='bg-[#A72211] w-full h-[600px] flex'>
-                    <div className="flex w-full text-white justify-center items-center pl-12">
+            <div className={`bg-black h-[828px] w-full flex justify-center items-center ${order === 'left' ? 'flex-row-reverse' : 'flex-row'}`}>
+                <div className={`bg-${color} w-full h-[600px] flex`}>
+                    <div className={`flex w-full text-white justify-center items-center  ${order === 'left' ? 'flex-row-reverse pr-12' : 'flex-row pl-12'}`}>
                         <div>
-                            <h1 className="text-[70px] font-Leaguespartan leading-[76.6%] font-bold mb-1">01</h1>
-                            <h1 className="text-[35px] font-Archivo italic font-[400] mb-1">Gold Key Concierge</h1>
-                            <div className='pr-10'>
-                                <p className="text-[14px] font-Archivo font-[300] leading-[19.8px]"> Masterpiece Limousine offers a premium level of service with our Gold Key Concierge Service. We go above and beyond to provide a personalized and tailored experience that caters to the specific needs and preferences of our most discerning clients. Our commitment to excellence and attention to detail make us the preferred choice for luxury ground transportation.</p>
-                                <br />
-                                <p className="text-[14px] font-Archivo font-[300] leading-[19.8px]">With our Gold Key Concierge Service, you can expect nothing but the best. Our experienced team of professionals works closely with you to create a customized itinerary that meets your specific needs and preferences. We provide expert advice and recommendations on event planning, entertainment options, restaurant reservations, and more. We strive to exceed your expectations and create an unforgettable travel experience.</p>
-                                <br />
-                                <p className="text-[14px] font-Archivo font-[300] leading-[19.8px]">At Masterpiece Limousine, we understand that luxury travel should be seamless and stress-free. Our Gold Key Concierge Service is available 24/7 to provide around-the-clock assistance and support. We are dedicated to providing the highest level of service to ensure that your travel experience is smooth and hassle-free. Contact us today to learn more about our Gold Key Concierge Service, and let us help you create an unforgettable travel experience.</p>
-                            </div>
+                            <h1 className={`text-[70px] font-Leaguespartan leading-[76.6%] font-bold mb-1 ${order === 'left' ? 'pl-12' : ''}`}>{`${number}`}</h1>
+                            <h1 className={`text-[35px] font-Archivo italic font-[400] mb-1 ${order === 'left' ? 'pl-12' : ''}`}>{`${title}`}</h1>
 
+                            <div className={`${order === 'left' ? 'pl-12' : 'pr-10'}`}>
+                                {textContent.map((text, index) => (
+                                    <React.Fragment key={index}>
+                                        <p className="text-[14px] font-Archivo font-[300] leading-[19.8px]">{text}</p>
+                                        {index < textContent.length - 1 && (number !== '17' ? <br /> : null)}
+                                    </React.Fragment>
+                                ))}
+                            </div>
                         </div>
                         <img
-                            src="/services/hero/01.png"
+                            src={`${imgURL}`}
                             alt=""
                             className="w-[420px] h-[660px]"
                         />
                     </div>
                 </div>
-                <div className="bg-[#A72211] w-[5%] h-[600px] flex justify-center items-center flex-col">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 34" fill="none" tyle={{ margin: '0px' }}>
-                        <path d="M1.99822 32.1138L2.22499 15.1476L20.0017 2.06227L19.775 19.0284L1.99822 32.1138Z" fill="white" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '-10px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 22 35" fill="none" style={{ margin: '0px' }}>
-                        <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" stroke="white" stroke-width="2" />
-                    </svg>
+                <div className={`bg-${color} w-[5%] h-[600px] flex justify-center items-center flex-col`}>
+                    {filledIcons.map((index) => (
+                        <svg
+                            key={index}
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="28"
+                            viewBox="0 0 22 34"
+                            fill="white"
+                            stroke="white"
+                            strokeWidth="2"
+                            style={{ margin: index % 2 === 0 ? '0px' : '-10px' }}
+                        >
+                            <path d="M1.99822 32.1138L2.22499 15.1476L20.0017 2.06227L19.775 19.0284L1.99822 32.1138Z" />
+                        </svg>
+                    ))}
+                    {unfilledIcons.map((index) => (
+                        <svg
+                            key={index}
+                            xmlns="http://w3.org/2000/svg"
+                            width="16"
+                            height="28"
+                            viewBox="0 0 22 34"
+                            fill="none"
+                            stroke="white"
+                            strokeWidth="2"
+                            style={{ margin: index % 2 === 0 ? '-10px' : '0px' }}
+                        >
+                            <path d="M1.99822 32.4644L2.22499 15.4983L20.0017 2.41292L19.775 19.379L1.99822 32.4644Z" />
+                        </svg>
+                    ))}
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Section
+export default Section;
